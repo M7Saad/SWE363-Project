cc5a9b07affc4eb90f5a950f2caacf22d1228a4d
Mohammed <84126236+M7Saad@users.noreply.github.com>
2023-12-07 11:51:02 +0300

    consultant info to db example

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,45 +1,68 @@
 <template>
-    <div id="app">
-      <header>
-        <h1>Mustashark</h1>
-        <nav>
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Contact</a>
-        </nav>
-      </header>
-  
-      <main>
-        <!--take input, one field-->
-        <input type="text" v-model="inputField" placeholder="Enter something">
-        <!--take input, one field-->
-        <button @click="pushToFirebase">Push to Firebase</button>
-      </main>
-  
-      <footer>
-        <p>© 2024 Mustashark. All rights reserved.</p>
-      </footer>
-    </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  import { getDatabase, ref as dbRef, set } from "firebase/database";
-  
-  export default {
-    data() {
-      return {
-        imageUrl: 'https://victoria.mediaplanet.com/app/uploads/sites/105/2019/08/MainImage_A5-2.jpg',
-        inputField: '',
-      };
+  <div id="app">
+    <header>
+      <h1>Mustashark</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+
+    <main>
+      <!--take input, one field-->
+      <input type="text" v-model="inputField" placeholder="Enter something" />
+      <!--take input, one field-->
+      <button @click="pushToFirebase(test)">Push to Firebase</button>
+    </main>
+
+    <footer>
+      <p>© 2024 Mustashark. All rights reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import { getDatabase, ref as dbRef, set } from "firebase/database";
+import Consultant from "../classes.js";
+
+export default {
+  data() {
+    return {
+      imageUrl:
+        "https://victoria.mediaplanet.com/app/uploads/sites/105/2019/08/MainImage_A5-2.jpg",
+      inputField: "",
+      test: new Consultant(
+        "Ali Afif",
+        "Financial Advisor, Career Counselor",
+        120,
+        "https://pbs.twimg.com/profile_images/1128330688976044032/-icjAsNR_400x400.jpg",
+        ["Certified Financial Planner", "MBA in Finance"],
+        ["Financial Advisor", "Career Counselor"]
+      ),
+    };
+  },
+  methods: {
+    async pushToFirebase(test) {
+      for (const key in test) {
+        console.log(key);
+        if (test[key] === undefined || test[key] === null) {
+          console.error(`The property '${key}' is ${test[key]}`);
+          return;
+        }
+      }
+      const db = getDatabase();
+      const consultantRef = dbRef(db, "consultants/" + test.name);
+      await set(consultantRef, {
+        name: test.name,
+        expertise: test.expertise,
+        price: test.price,
+        photo: test.photo,
+        qualifications: test.qualifications,
+        consultancyTypes: test.consultancyTypes,
+      });
+      console.log("Consultant stored in Firebase!");
     },
-    methods: {
-      async pushToFirebase() {
-        const db = getDatabase();
-        const inputRef = dbRef(db, "users/" +"123");  // Change 'inputField' to the path where you want to store the data
-        await set(inputRef, this.inputField);
-        console.log('Data stored in Firebase!');
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
