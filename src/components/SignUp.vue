2aa55ec47b98d35be1b39d91aa05622677fc3e59
aliafif <aliafif6001@gmail.com>
2023-12-07 22:00:48 +0300

    edit Signup page

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,70 +1,75 @@
 <template>
-  <header>
-    <h1>Mustashark</h1>
-    <nav>
-      <a href="#">Home</a>
-      <a href="#">About</a>
-      <a href="#">Contact</a>
-    </nav>
-  </header>
   <div>
-    <h2>Sign up</h2>
-    <form @submit.prevent="signUp">
-      <div class="form-floating mb-3">
-        <input
-          name="name"
-          type="text"
-          class="form-control"
-          id="name"
-          placeholder="name"
-          required
-        />
-        <label for="name">Name</label>
+    <header>
+      <h1>Mustashark</h1>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+    <main>
+      <div class="signup-header">
+        <h2>Sign up</h2>
       </div>
-      <div class="form-floating mb-3">
-        <input
-          name="photoURL"
-          type="link"
-          class="form-control"
-          id="photoURL"
-          placeholder="photoURL"
-        />
-        <label for="photoURL">photoURL (Optional)</label>
+      <div class="signup-container">
+        
+        <form @submit.prevent="signUp" class="signup-form">
+          <div class="form-floating mb-3">
+            <input
+              name="name"
+              type="text"
+              class="form-control"
+              id="name"
+              placeholder="name"
+              required
+            />
+            <label for="name">Name</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              name="photoURL"
+              type="link"
+              class="form-control"
+              id="photoURL"
+              placeholder="photoURL"
+            />
+            <label for="photoURL">photoURL (Optional)</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              name="email"
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder="name@example.com"
+              required
+            />
+            <label for="email">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input
+              name="password"
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <label for="password">Password</label>
+          </div>
+          <br />
+          <button type="submit">Sign up</button>
+        </form>
+        <div class="login-link">
+          <router-link to="/login">Already have a Mustasharak account? Login</router-link>
+        </div>
       </div>
-      <div class="form-floating mb-3">
-        <input
-          name="email"
-          type="email"
-          class="form-control"
-          id="email"
-          placeholder="name@example.com"
-          required
-        />
-        <label for="email">Email address</label>
-      </div>
-      <div class="form-floating">
-        <input
-          name="password"
-          type="password"
-          class="form-control"
-          id="password"
-          placeholder="Password"
-          required
-        />
-        <label for="password">Password</label>
-      </div>
-      <br />
-      <button type="submit">Sign up</button>
-    </form>
-    <br />
-    <br />
-    <router-link to="/login"
-      >Already have a Mustasharak account? login</router-link
-    >
+    </main>
+    <footer>
+      <p>© 2024 Mustashark. All rights reserved.</p>
+    </footer>
   </div>
-  <footer>
-    <p>© 2024 Mustashark. All rights reserved.</p>
-  </footer>
 </template>
 
 <script>
@@ -134,6 +139,45 @@ nav a {
   text-decoration: none;
   text-transform: uppercase;
 }
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 140px);
+}
+
+.signup-container {
+  width: 1500px;
+  height: 700px;
+  margin: auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.signup-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-floating {
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 5px;
+  display: block;
+}
+
+input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 100%;
+  box-sizing: border-box;
+}
 
 button {
   background-color: indigo;
@@ -142,6 +186,12 @@ button {
   padding: 10px 20px;
   text-transform: uppercase;
   cursor: pointer;
+  border-radius: 4px;
+}
+
+.login-link {
+  margin-top: 20px;
+  text-align: center;
 }
 
 footer {
@@ -152,5 +202,10 @@ footer {
   position: fixed;
   bottom: 0;
   width: 100%;
+}
+.signup-header {
+  text-align: center;
+  margin-bottom: 20px;
+  padding: 10px;
 }
 </style>
