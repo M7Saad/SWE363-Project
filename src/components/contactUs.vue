42ed8a2a7d71b024dff0125ce4e95a7ba88e9ea9
Mohammed <84126236+M7Saad@users.noreply.github.com>
2023-12-08 02:33:30 +0300

    CONNECTED TO THE DB, USED NAVBAR

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,109 +1,165 @@
 <template>
-	<header>
-		<h1>Mustashark</h1>
-		<nav>
-			<a href="#">Home</a>
-			<a href="#">About</a>
-			<a href="#">Contact</a>
-		</nav>
-	</header>
-	
-	<section id="contact">
-  
-  <h1 class="section-header">Contact</h1>
-  
-  <div class="contact-wrapper">
-  
-    
-    <form id="contact-form" class="form-horizontal" role="form">
-       
-      <div class="form-group">
-        <div class="col-sm-12">
-          <input type="text" class="form-control" id="name" placeholder="Enter your name" name="name" value="" required>
-        </div>
-      </div>
+  <Navbar />
 
-      <div class="form-group">
-        <div class="col-sm-12">
-          <input type="email" class="form-control" id="email" placeholder="Enter your email" name="email" value="" required>
-        </div>
-      </div>
+  <section id="contact">
+    <h1 class="section-header">Contact</h1>
 
-      <textarea class="form-control" rows="10" placeholder="Write your message" name="message" required></textarea>
-      
-      <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
-        <div class="alt-send-button">
-          <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
+    <div class="contact-wrapper">
+      <form
+        @submit.prevent="createContactMsg"
+        id="contact-form"
+        class="form-horizontal"
+        role="form"
+      >
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              placeholder="Enter your name"
+              name="name"
+              v-model="name"
+              required
+            />
+          </div>
         </div>
-      
-      </button>
-      
-    </form>
-    
-    
+
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder="Enter your email"
+              name="email"
+              v-model="email"
+              required
+            />
+          </div>
+        </div>
+
+        <textarea
+          class="form-control"
+          rows="10"
+          placeholder="Write your message"
+          name="message"
+          required
+          v-model="msg"
+        ></textarea>
+
+        <button
+          class="btn btn-primary send-button"
+          id="submit"
+          type="submit"
+          value="SEND"
+        >
+          <div class="alt-send-button">
+            <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
+          </div>
+        </button>
+      </form>
+
       <div class="direct-contact-container">
-
         <ul class="contact-list">
-          <li class="list-item"><span class="contact-text place">dhahran, KSA</span></li>
-          
-          <li class="list-item"><span class="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">(012) 675-5576</a></span></li>
-          
-          <li class="list-item"><span class="contact-text gmail"><a href="mailto:Mustashark@gmail.com" title="Send me an email">Mustashark@gmail.com</a></span></li>
-          
+          <li class="list-item">
+            <span class="contact-text place">dhahran, KSA</span>
+          </li>
+
+          <li class="list-item">
+            <span class="contact-text phone"
+              ><a href="tel:1-212-555-5555" title="Give me a call"
+                >(012) 675-5576</a
+              ></span
+            >
+          </li>
+
+          <li class="list-item">
+            <span class="contact-text gmail"
+              ><a href="mailto:Mustashark@gmail.com" title="Send me an email"
+                >Mustashark@gmail.com</a
+              ></span
+            >
+          </li>
         </ul>
 
-        <hr>
+        <hr />
         <ul class="social-media-list">
-          <li><a href="#" target="_blank" class="contact-icon">
-            </a>
-          </li>
-          <li><a href="#" target="_blank" class="contact-icon">
-            </a>
-          </li>
-          <li><a href="#" target="_blank" class="contact-icon">
-            </a>
-          </li>       
+          <li><a href="#" target="_blank" class="contact-icon"> </a></li>
+          <li><a href="#" target="_blank" class="contact-icon"> </a></li>
+          <li><a href="#" target="_blank" class="contact-icon"> </a></li>
         </ul>
-
-
       </div>
-    
-  </div>
-  
-</section>
+    </div>
+  </section>
 
-	<footer>
-		<p>© 2024 Mustashark. All rights reserved.</p>
-	</footer>
+  <footer>
+    <p>© 2024 Mustashark. All rights reserved.</p>
+  </footer>
 </template>
-  
 
+<script>
+import { ContactMsg } from "../classes.js";
+import { getDatabase, ref as dbRef, set } from "firebase/database";
+export default {
+  data() {
+    return {
+      name: "",
+      msg: "",
+      email: "",
+    };
+  },
+  methods: {
+    createContactMsg() {
+      console.log(this.email);
+      const msg = new ContactMsg(this.name, this.email, this.message);
+      console.log(msg);
+      //upload to firebase
+      this.pushToFirebase();
+    },
+    async pushToFirebase(test) {
+      console.log(this.email);
+      const db = getDatabase();
+      const msgRef = dbRef(db, "contactmsgs/" + this.name); //TODO :CHANGE TO USERID
+      await set(msgRef, {
+        name: this.name,
+        email: this.email,
+        message: this.msg,
+      });
+      console.log("msg has sent to the db");
+    },
+  },
+};
+</script>
 
+<script setup>
+import Navbar from "./Navbar.vue";
+</script>
 <style>
 header {
-	background-color: indigo;
-	color: white;
-	display: flex;
-	justify-content: space-between;
-	padding: 20px;
-	align-items: center;
+  background-color: indigo;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  align-items: center;
 }
 
 nav a {
-	margin: 0 10px;
-	color: white;
-	text-decoration: none;
-	text-transform: uppercase;
+  margin: 0 10px;
+  color: white;
+  text-decoration: none;
+  text-transform: uppercase;
 }
 
 footer {
-	background-color: indigo;
-	color: white;
-	text-align: center;
-	padding: 10px;
-	position: fixed;
-	bottom: 0;
-	width: 100%;
+  background-color: indigo;
+  color: white;
+  text-align: center;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 }
 body {
   margin: 0;
@@ -120,7 +176,7 @@ body {
   text-align: center;
   margin: 0 auto;
   padding: 40px 0;
-  font: 300 80px 'Oswald', sans-serif;
+  font: 300 80px "Oswald", sans-serif;
   text-transform: uppercase;
   letter-spacing: 6px;
 }
@@ -138,16 +194,16 @@ body {
 
 .form-horizontal {
   max-width: 400px;
-  font-family: 'Lato';
+  font-family: "Lato";
   font-weight: 400;
 }
 
-.form-control, 
+.form-control,
 textarea {
   max-width: 700px;
   background-color: #000;
   letter-spacing: 1px;
-  font-size: 24px; 
+  font-size: 24px;
   padding: 20px;
 }
 
@@ -156,25 +212,24 @@ textarea {
   height: 60px;
   width: 500px;
   overflow: hidden;
-  transition: all .2s ease-in-out;
+  transition: all 0.2s ease-in-out;
 }
 
 .alt-send-button {
   width: 400px;
   height: 50px;
-  transition: all .2s ease-in-out;
+  transition: all 0.2s ease-in-out;
   align-items: center;
 }
 
 .send-text {
-  display: flex; 
+  display: flex;
   justify-content: center;
   display: block;
   margin-top: 10px;
-  font: 700 20px 'Lato', sans-serif;
+  font: 700 20px "Lato", sans-serif;
   letter-spacing: 2px;
   font-size: 30px;
-  
 }
 .alt-send-button:hover {
   transform: translate3d(0px, -29px, 0px);
@@ -196,7 +251,7 @@ textarea {
 }
 
 .contact-text {
-  font: 300 20px 'Lato', sans-serif;
+  font: 300 20px "Lato", sans-serif;
   letter-spacing: 1.9px;
 }
 
@@ -221,7 +276,6 @@ textarea {
   text-decoration: none;
 }
 
-
 .social-media-list {
   position: relative;
   font-size: 22px;
@@ -231,22 +285,21 @@ textarea {
   padding: 0;
 }
 
-
 .social-media-list li {
-  position: relative; 
+  position: relative;
   display: inline-block;
   height: 60px;
   width: 60px;
   margin: 10px 3px;
   line-height: 60px;
   border-radius: 50%;
-  background-color: rgb(27,27,27);
-  cursor: pointer; 
-  transition: all .2s ease-in-out;
+  background-color: rgb(27, 27, 27);
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
 }
 
 .social-media-list li:after {
-  content: '';
+  content: "";
   position: absolute;
   top: 0;
   left: 0;
@@ -256,15 +309,13 @@ textarea {
   border-radius: 50%;
   opacity: 0;
   box-shadow: 0 0 0 1px black;
-  transition: all .2s ease-in-out;
+  transition: all 0.2s ease-in-out;
 }
 
-
-
 .social-media-list li:hover:after {
-  opacity: 1;  
+  opacity: 1;
   transform: scale(1.12);
-  transition-timing-function: cubic-bezier(0.37,0.74,0.15,1.65);
+  transition-timing-function: cubic-bezier(0.37, 0.74, 0.15, 1.65);
 }
 
 .social-media-list li:hover a {
@@ -280,14 +331,15 @@ hr {
     display: flex;
     flex-direction: column;
   }
-  .direct-contact-container, .form-horizontal {
+  .direct-contact-container,
+  .form-horizontal {
     margin: 0 auto;
-  }  
-  
+  }
+
   .direct-contact-container {
     margin-top: 60px;
     max-width: 300px;
-  }    
+  }
   .social-media-list li {
     height: 60px;
     width: 60px;
@@ -301,25 +353,26 @@ hr {
 }
 
 @media screen and (max-width: 569px) {
-
-  .direct-contact-container, .form-wrapper {
+  .direct-contact-container,
+  .form-wrapper {
     float: none;
     margin: 0 auto;
-  }  
-  .form-control, textarea {
-    
+  }
+  .form-control,
+  textarea {
     margin: 0 auto;
   }
- 
-  
-  .name, .email, textarea {
+
+  .name,
+  .email,
+  textarea {
     width: 280px;
-  } 
-  
+  }
+
   .direct-contact-container {
     margin-top: 60px;
     max-width: 280px;
-  }  
+  }
   .social-media-list {
     left: 0;
   }
@@ -334,7 +387,6 @@ hr {
     height: 55px;
     line-height: 55px;
   }
-  
 }
 
 @media screen and (max-width: 410px) {
@@ -342,21 +394,20 @@ hr {
     width: 99%;
   }
 }
-#submit{
+#submit {
   background-color: indigo;
   color: #000;
 }
 .phone a,
 .gmail a {
-  color: rgb(1, 1, 30); 
+  color: rgb(1, 1, 30);
 }
 #contact-form input[type="text"],
 #contact-form input[type="email"],
 #contact-form textarea {
-  width: 500px; 
-  font-size: 25px; 
-  padding: 15px; 
-  margin-bottom: 15px; 
+  width: 500px;
+  font-size: 25px;
+  padding: 15px;
+  margin-bottom: 15px;
 }
-
 </style>
