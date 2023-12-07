6d590b01b7f4fcfbcde83aa2af06cc418003b4e3
Mohammed <84126236+M7Saad@users.noreply.github.com>
2023-12-07 09:47:57 +0300

    consultant class

---
+++ src/classes.js
@@ -0,0 +1,10 @@
+class Consultant {
+  constructor(name, expertise, price, photo, qualifications, consultancyTypes) {
+    this.name = name;
+    this.expertise = expertise;
+    this.price = price;
+    this.photo = photo;
+    this.qualifications = qualifications;
+    this.consultancyTypes = consultancyTypes;
+  }
+}
