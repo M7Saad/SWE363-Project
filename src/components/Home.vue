bae5879ff7955586b0a1aeab952e3b2c891c34b7
Ahmed AL Mughirah <s202018480@kfupm.edu.sa>
2023-12-08 20:49:43 +0300

    LoginSiginHomeUpdate

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="container py-5">
         <div class="row">
-          <div class="col-lg-6">
+          <div class="col-lg-6 info">
             <h1>Welcome to Mustashark!</h1>
             <p>
               Mustashark is your comprehensive financial consultancy web
@@ -53,6 +53,20 @@
             </div>
           </div>
         </div>
+
+        <div class="row mt-5">
+          <div class="col-12">
+            <h2 class="head">Our Partners</h2>
+            <div class="partners">
+              <transition-group name="fade" tag="div" class="partners-row">
+                <div v-for="(partner, index) in partners" :key="index" class="partner">
+                  <img :src="partner.logoUrl" :alt="partner.name" class="img-fluid" />
+                </div>
+              </transition-group>
+            </div>
+          </div>
+        </div>
+
       </div>
     </main>
     <Footer />
@@ -65,6 +79,11 @@ export default {
     return {
       imageUrl:
         "https://victoria.mediaplanet.com/app/uploads/sites/105/2019/08/MainImage_A5-2.jpg",
+      partners: [
+        { name: 'Partner1', logoUrl: 'https://upload.wikimedia.org/wikipedia/ar/archive/3/37/20180719130502%21King_Fahd_University_of_Petroleum_%26_Minerals_Logo.png' },
+        { name: 'Partner2', logoUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png' },
+        // Add more partners as needed
+      ],
     };
   },
   methods: {
@@ -89,8 +108,23 @@ body,
   height: 100%;
 }
 
+.info {
+  margin-left: 50px;
+  margin-bottom: 100px;
+
+  width: 500px;
+}
+
 main {
   padding: 20px;
+}
+
+img {
+  width: 400px;
+  padding: 0;
+  margin-right: 10px;
+  margin-bottom: 30px;
+
 }
 
 .row {
@@ -110,13 +144,6 @@ main {
 .image-section {
   transition: transform 1s ease-in-out;
 }
-.text-section {
-  transition: opacity 1s ease-in-out;
-}
-
-.image-section {
-  transition: transform 1s ease-in-out;
-}
 
 .fade-enter-active,
 .fade-leave-active {
@@ -127,4 +154,34 @@ main {
 .fade-leave-to {
   opacity: 0;
 }
-</style>
+
+.card-title {
+  text-align: center;
+}
+
+.card-text {
+  text-align: center;
+  color: grey;
+}
+
+.partners {
+  margin-top: 2rem;
+  text-align: center;
+}
+
+.partners-row {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+}
+
+.partner {
+  width: 150px;
+}
+
+.head {
+  margin-top: 30px;
+  text-align: center;
+}</style>
+
