b4c02bf0a731d67ed432ed78f2429cb6b977bb4a
aliafif <aliafif6001@gmail.com>
2023-12-09 18:40:48 +0300

    edit the consultant page

--- src/components/Consultant.vue
+++ src/components/Consultant.vue
@@ -2,42 +2,106 @@
   <div id="app">
     <Navbar />
 
-    <div class="content">
-      <form id="add_offer">
-        <h1>Add new consulting offer</h1>
-        <div class="col">
-          <label for="message">Description:</label>
-          <textarea id="description" rows="4" cols="50" required></textarea>
-          <br />
-          <div>
-            <label for="number">Your price per hour:</label>
+    <div class="main-container">
+
+      <div class="offer-container">
+        <h2>Add new consulting offer</h2>
+        <form class="offer-form">
+          <div class="form-floating mb-3">
             <input
-              type="number"
-              id="price"
-              min="100"
-              max="3000"
-              placeholder="300$"
+              description="description"
+              type="text"
+              class="form-control"
+              id="description"
+              placeholder="Your description"
               required
             />
+            <label for="description">Description:</label>
           </div>
-          <label for="type">Types:</label>
-          <select id="types" required>
-            <option value="financial">Financial Consulting</option>
-            <option value="human resources">Human Resources Consulting</option>
-            <option value="strategy">Strategy Consulting</option>
-            <option value="information technology">
-              Information Technology Consulting
-            </option>
-            <option value="operations">Operations Consulting</option>
-          </select>
-          <br />
-          <input type="submit" value="Add" />
-        </div>
-      </form>
+          <div class="form-floating mb-3">
+            <input
+              name="Experience"
+              type="text"
+              class="form-control"
+              id="Experience"
+              placeholder="Your experience"
+              required
+            />
+            <label for="Experience">Experience:</label>
+          </div>
 
-      <div id="consultation">
-        <h1 id="cases">Consultation Cases</h1>
-        <div id="cases_box"></div>
+          <div v-for="(qualification, index) in qualifications" :key="index" class="qualification-row">
+            <div class="form-floating mb-3">
+              <input
+                type="text"
+                class="form-control"
+                :id="'qualification' + index"
+                :placeholder="'Qualifications ' + (index + 1)"
+                v-model="qualification.text"
+                required
+              />
+              <label :for="'qualification' + index">Qualifications:</label>
+            </div>
+            <button type="button" @click="removeQualification(index)">Remove</button>
+          </div>
+
+          <div class="form-floating mb-3">
+            <button type="button" @click="addQualification">Add Qualifications</button>
+          </div>
+
+          <div class="form-floating mb-3">
+            <select
+              name="consultantType"
+              class="form-control"
+              id="consultantType"
+              required
+            >
+              <option value="" disabled selected>Select Consultant Type</option>
+              <option value="Financial Consulting">Financial Consulting</option>
+              <option value="Human Resources">Human Resources</option>
+              <option value="Strategy Consulting">Strategy Consulting</option>
+              <option value="Information Technology Consulting">Information Technology Consulting</option>
+              <option value="Operations Consulting">Operations Consulting</option>
+            </select>
+            <label for="consultantType">Consultant Type:</label>
+          </div>
+          <div class="form-floating mb-3">
+            <input
+              name="price"
+              type="number"
+              class="form-control"
+              id="price"
+              placeholder="Enter your Price per hour"
+              required
+              min="100"
+              max="3000"
+            />
+            <label for="price">Price:</label>
+          </div>
+          <div class="form-floating mb-3">
+            <br />
+            <input type="submit" value="Add" class="add-offer" @click.prevent="submitForm" />
+          </div>
+        </form>
+      </div>
+
+      <h1 id="cases">Consultation Cases</h1>
+      <div class="cases-box">
+        <div class="cases-card" v-for="(request, index) in requests" :key="index">
+          <strong>Name:</strong><br />
+          <p class="name">{{ request.user_name }}</p>
+          <hr>
+          <strong>Phone:</strong><br />
+          <a :href="request.Phone_number">Call</a>
+          <div class="status-dropdown">
+            <label for="status">Status:</label>
+            <select v-model="request.status" @change="updateStatus(request, index)">
+              <option value="in_progress">In Progress</option>
+              <option value="rejected">Reject</option>
+              <option value="finished">Finished</option>
+            </select>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -47,134 +111,55 @@
 
 <script>
 export default {
-  mounted() {
-    const addCase = (name, phoneNumber) => {
-      var casesBox = document.getElementById("cases_box");
+  data() {
+    return {
+      qualifications: [{ text: '' }],
 
-      var caseElement = document.createElement("div");
-      caseElement.className = "case";
-
-      caseElement.innerHTML =
-        "<strong>Name:</strong> " +
-        name +
-        "<br><strong>Phone:</strong> " +
-        phoneNumber;
-
-      var selectBox = document.createElement("div");
-      selectBox.className = "select-box";
-
-      var selectElement = document.createElement("select");
-
-      var statuses = ["In Progress", "Rejected", "Finished"];
-      statuses.forEach(function (status) {
-        var option = document.createElement("option");
-        option.value = status.toLowerCase().replace(/\s/g, "-");
-        option.text = status;
-        selectElement.add(option);
-      });
-
-      selectBox.appendChild(selectElement);
-      caseElement.appendChild(selectBox);
-      casesBox.appendChild(caseElement);
-      casesBox.appendChild(document.createElement("hr"));
+      requests: [
+        {
+          user_name: "Ali Ahmed",
+          Phone_number: "34455666",
+        },
+        {
+          user_name: "Ahmed Ali",
+          Phone_number: "34455666",
+        },
+        {
+          user_name: "Ahmed Ali",
+          Phone_number: "34455666",
+        },
+      ],
     };
 
-    addCase("Ali Ahmed", "0345533555");
-    addCase("Zahid Sami", "055545505");
+  },
+  methods: {
+    addQualification() {
+      this.qualifications.push({ text: '' });
+    },
+    removeQualification(index) {
+      this.qualifications.splice(index, 1);
+    },
+    submitForm() {
+      console.log('Form submitted:', this.qualifications);
+    },
+    updateStatus(request, index) {
+      console.log(`Updating status for ${request.user_name} at index ${index}`);
+    },
   },
 };
 </script>
+
 <script setup>
 import Navbar from "./Navbar.vue";
 import Footer from "./Footer.vue";
 </script>
 
 <style scoped>
-.content {
+.main-container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-}
-
-#add_offer {
-  margin-bottom: 50px;
-}
-
-.form-container {
-  text-align: center;
-}
-
-.offer-container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-}
-
-.columns {
-  display: grid;
-  grid-gap: 15px;
-}
-
-.form-group {
-  margin-bottom: 15px;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input[type="submit"] {
-  padding: 5px 30px;
-  background-color: #254d26;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-top: 10px;
-}
-
-#cases {
-  margin-top: 50px;
-}
-
-#cases_box {
-  background-color: gainsboro;
-  border: solid 1px;
-  height: 400px;
-  width: 1142px;
-  overflow: auto;
-  margin: 0 auto;
-}
-
-.case {
-  border: solid 1px;
-  padding: 10px;
-  margin: 5px;
-}
-
-html,
-body,
-#app {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
-
-main {
-  padding: 20px;
-}
-
-.row {
-  display: flex;
-  justify-content: space-between;
-}
-
-.col {
-  flex: 1;
 }
 
 button {
@@ -186,8 +171,56 @@ button {
   cursor: pointer;
 }
 
-img {
-  max-width: 100%;
-  height: auto;
+.qualification-row {
+  display: flex;
+  align-items: center;
+}
+
+.qualification-row button {
+  margin-left: 10px;
+}
+
+.offer-container {
+  width: 400px;
+  padding: 20px;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+.cases-box {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+  
+}
+
+.cases-card {
+  width: 200px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 10px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 15px;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+.add-offer {
+  background-color: indigo; 
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.add-offer:hover {
+  background-color: rgb(137, 71, 184); 
 }
 </style>
