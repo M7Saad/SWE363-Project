730f086733d854c83b962faedc92568d295db544
aliafif <aliafif6001@gmail.com>
2023-12-13 17:14:35 +0300

    edit the contact us page

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,148 +1,70 @@
 <template>
-  <Navbar />
+  <div>
+    <Navbar />
 
-  <section id="contact">
-    <h1 class="section-header">Contact</h1>
+    <section id="contact" class="contact-section">
+      <div class="container">
+        <h1 class="section-header"><strong>Contact us</strong></h1>
 
-    <div class="contact-wrapper">
-      <form
-        @submit.prevent="createContactMsg"
-        id="contact-form"
-        class="form-horizontal"
-        role="form"
-      >
-        <div class="form-group">
-          <div class="col-sm-12">
-            <input
-              type="text"
-              class="form-control"
-              id="name"
-              placeholder="Enter your name"
-              name="name"
-              v-model="name"
-              required
-            />
+        <div class="contact-wrapper">
+          <form @submit.prevent="createContactMsg" id="contact-form" class="form-horizontal" role="form">
+            <div class="form-group">
+              <input type="text" class="form-control" id="name" placeholder="Your Name" name="name" v-model="name" required />
+            </div>
+
+            <div class="form-group">
+              <input type="email" class="form-control" id="email" placeholder="Your Email" name="email" v-model="email" required />
+            </div>
+
+            <textarea class="form-control" rows="8" placeholder="How can we assist you?" name="message" required v-model="msg"></textarea>
+
+            <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+              <div class="alt-send-button">
+                <i class="fa fa-paper-plane"></i><span class="send-text"><strong>Send Message</strong></span>
+              </div>
+            </button>
+          </form>
+
+          <div class="direct-contact-container">
+            <ul class="contact-list">
+              <li class="list-item">
+                <span class="contact-text place"><strong>Dhahran, KSA</strong></span>
+              </li>
+
+              <li class="list-item">
+                <span class="contact-text phone">
+                  <a href="tel:012-675-5576" title="Give me a call"><strong>(012) 675-5576</strong></a>
+                </span>
+              </li>
+
+              <li class="list-item">
+                <span class="contact-text gmail">
+                  <a href="mailto:Mustashark@gmail.com" title="Send me an email"><strong>Mustashark@gmail.com</strong></a>
+                </span>
+              </li>
+            </ul>
+
+            <hr />
+
+            <ul class="social-media-list">
+              <li>
+                <a href="https://twitter.com/mustashark" target="_blank" class="contact-icon">
+                  <img src="./assets/twitter.gif" alt="Twitter" />
+                </a>
+              </li>
+              <li>
+                <a href="https://wa.me/1234567890" target="_blank" class="contact-icon">
+                  <img src="./assets/whatsapp.gif" alt="WhatsApp" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-
-        <div class="form-group">
-          <div class="col-sm-12">
-            <input
-              type="email"
-              class="form-control"
-              id="email"
-              placeholder="Enter your email"
-              name="email"
-              v-model="email"
-              required
-            />
-          </div>
-        </div>
-
-        <textarea
-          class="form-control"
-          rows="10"
-          placeholder="Write your message"
-          name="message"
-          required
-          v-model="msg"
-        ></textarea>
-
-        <button
-          class="btn btn-primary send-button"
-          id="submit"
-          type="submit"
-          value="SEND"
-        >
-          <div class="alt-send-button">
-            <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
-          </div>
-        </button>
-      </form>
-
-      <div class="direct-contact-container">
-        <ul class="contact-list">
-          <li class="list-item">
-            <span class="contact-text place">dhahran, KSA</span>
-          </li>
-
-          <li class="list-item">
-            <span class="contact-text phone"
-              ><a href="tel:1-212-555-5555" title="Give me a call"
-                >(012) 675-5576</a
-              ></span
-            >
-          </li>
-
-          <li class="list-item">
-            <span class="contact-text gmail"
-              ><a href="mailto:Mustashark@gmail.com" title="Send me an email"
-                >Mustashark@gmail.com</a
-              ></span
-            >
-          </li>
-        </ul>
-
-        <!-- ... existing code ... -->
-
-        <hr />
-        <ul class="social-media-list">
-          <li>
-            <a href="https://facebook.com/mustashark"
-              ><img src="./assets/facebook.gif" alt="Facebook"
-            /></a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/mustashark"
-              target="_blank"
-              class="contact-icon"
-              ><img src="./assets/twitter.gif" alt="Twitter"
-            /></a>
-          </li>
-          <li>
-            <a
-              href="https://instagram.com/mustashark"
-              target="_blank"
-              class="contact-icon"
-              ><img src="./assets/instagram.gif" alt="Instagram"
-            /></a>
-          </li>
-          <hr />
-          <li>
-            <a
-              href="https://tiktok.com/@mustashark"
-              target="_blank"
-              class="contact-icon"
-              ><img src="./assets/tiktok.gif" alt="TikTok"
-            /></a>
-          </li>
-          <li>
-            <a
-              href="https://youtube.com/c/mustashark"
-              target="_blank"
-              class="contact-icon"
-              ><img src="./assets/youtube.gif" alt="YouTube"
-            /></a>
-          </li>
-          <li>
-            <a
-              href="https://wa.me/1234567890"
-              target="_blank"
-              class="contact-icon"
-              ><img src="./assets/whatsapp.gif" alt="WhatsApp"
-            /></a>
-          </li>
-        </ul>
-
-        <!-- ... existing code ... -->
-
-        <!-- ... existing code ... -->
       </div>
-    </div>
-  </section>
+    </section>
 
-  <Footer />
+    <Footer />
+  </div>
 </template>
 
 <script>
@@ -184,260 +106,132 @@ import Navbar from "./Navbar.vue";
 import Footer from "./Footer.vue";
 </script>
 
-<style>
-body {
-  margin: 0;
-  padding: 0;
-  padding-bottom: 100px;
-}
+<style scoped>
 
-#contact {
-  width: 100%;
-  height: 100%;
-}
+  body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
 
-.section-header {
-  text-align: center;
-  margin: 0 auto;
-  padding: 40px 0;
-  font: 300 80px "Oswald", sans-serif;
-  text-transform: uppercase;
-  letter-spacing: 6px;
-}
+  section#contact {
+    padding: 40px 0;
+  }
 
-.contact-wrapper {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin: 0 auto;
-  padding: 20px;
-  position: relative;
-  max-width: 840px;
-  font-size: 24px;
-}
+  h1.section-header {
+    text-align: center;
+    font-size: 2.5em;
+    color: #333;
+    margin-bottom: 30px;
+  }
+  .section-header {
+    text-align: center;
+    font-size: 2.5em;
+    color: #333;
+    margin-bottom: 30px;
+  }
 
-.form-horizontal {
-  max-width: 400px;
-  font-family: "Lato";
-  font-weight: 400;
-}
-
-.form-control,
-textarea {
-  max-width: 700px;
-  background-color: #000;
-  letter-spacing: 1px;
-  font-size: 24px;
-  padding: 20px;
-}
-
-.send-button {
-  margin-top: 15px;
-  height: 60px;
-  width: 500px;
-  overflow: hidden;
-  transition: all 0.2s ease-in-out;
-}
-
-.alt-send-button {
-  width: 400px;
-  height: 50px;
-  transition: all 0.2s ease-in-out;
-  align-items: center;
-}
-
-.send-text {
-  display: flex;
-  justify-content: center;
-  display: block;
-  margin-top: 10px;
-  font: 700 20px "Lato", sans-serif;
-  letter-spacing: 2px;
-  font-size: 30px;
-}
-.alt-send-button:hover {
-  transform: translate3d(0px, -29px, 0px);
-}
-
-.direct-contact-container {
-  max-width: 600px;
-}
-
-.contact-list {
-  list-style-type: none;
-  margin-left: -100px;
-  padding-right: 20px;
-  font-size: 24px;
-}
-
-.list-item {
-  line-height: 4;
-}
-
-.contact-text {
-  font: 300 20px "Lato", sans-serif;
-  letter-spacing: 1.9px;
-}
-
-.place {
-  margin-left: 62px;
-}
-
-.phone {
-  margin-left: 56px;
-}
-
-.gmail {
-  margin-left: 53px;
-}
-
-.contact-text a {
-  text-decoration: none;
-  transition-duration: 0.2s;
-}
-
-.contact-text a:hover {
-  text-decoration: none;
-}
-
-.social-media-list {
-  position: relative;
-  font-size: 22px;
-  text-align: center;
-  width: 100%;
-  margin: 0 auto;
-  padding: 0;
-}
-
-.social-media-list li {
-  position: relative;
-  display: inline-block;
-  height: 60px;
-  width: 60px;
-  margin: 10px 3px;
-  line-height: 60px;
-  border-radius: 50%;
-  background-color: rgb(27, 27, 27);
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-}
-
-.social-media-list li:after {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 60px;
-  height: 60px;
-  line-height: 60px;
-  border-radius: 50%;
-  opacity: 0;
-  box-shadow: 0 0 0 1px black;
-  transition: all 0.2s ease-in-out;
-}
-
-.social-media-list li:hover:after {
-  opacity: 1;
-  transform: scale(1.12);
-  transition-timing-function: cubic-bezier(0.37, 0.74, 0.15, 1.65);
-}
-
-.social-media-list li:hover a {
-  color: #000;
-}
-
-hr {
-  border-color: black;
-}
-
-@media screen and (max-width: 850px) {
   .contact-wrapper {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+  }
+  .contact-section {
+    padding: 60px 0;
+  }
+
+  form {
+    margin-bottom: 20px;
+  }
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+  }
+
+  .form-control {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 1em;
+  }
+
+  textarea.form-control {
+    resize: vertical;
+  }
+
+  .btn-primary {
+    background-color: rgb(1, 89, 88);
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  .alt-send-button {
     display: flex;
-    flex-direction: column;
+    align-items: center;
   }
-  .direct-contact-container,
-  .form-horizontal {
-    margin: 0 auto;
+
+  .fa-paper-plane {
+    margin-right: 10px;
   }
 
   .direct-contact-container {
-    margin-top: 60px;
-    max-width: 300px;
-  }
-  .social-media-list li {
-    height: 60px;
-    width: 60px;
-    line-height: 60px;
-  }
-  .social-media-list li:after {
-    width: 60px;
-    height: 60px;
-    line-height: 60px;
-  }
-}
-
-@media screen and (max-width: 569px) {
-  .direct-contact-container,
-  .form-wrapper {
-    float: none;
-    margin: 0 auto;
-  }
-  .form-control,
-  textarea {
-    margin: 0 auto;
+    margin-top: 20px;
   }
 
-  .name,
-  .email,
-  textarea {
-    width: 280px;
+  .contact-list {
+    list-style: none;
+    padding: 0;
   }
 
-  .direct-contact-container {
-    margin-top: 60px;
-    max-width: 280px;
+  .list-item {
+    margin-bottom: 10px;
   }
+
+  .contact-text {
+    display: block;
+    font-size: 1.1em;
+    color: #555;
+  }
+
+  .contact-icon img {
+    width: 40px;
+    height: 40px;
+    margin-right: 10px;
+  }
+
+  hr {
+    border: 0;
+    border-top: 1px solid #ccc;
+    margin: 20px 0;
+  }
+
   .social-media-list {
-    left: 0;
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
   }
-  .social-media-list li {
-    height: 55px;
-    width: 55px;
-    line-height: 55px;
-    font-size: 2rem;
-  }
-  .social-media-list li:after {
-    width: 55px;
-    height: 55px;
-    line-height: 55px;
-  }
-}
 
-@media screen and (max-width: 410px) {
-  .send-button {
-    width: 99%;
+  .social-media-list li {
+    margin-right: 20px;
   }
-}
-#submit {
-  background-color: indigo;
-  color: #000;
-}
-.phone a,
-.gmail a {
-  color: rgb(1, 1, 30);
-}
-#contact-form input[type="text"],
-#contact-form input[type="email"],
-#contact-form textarea {
-  width: 500px;
-  font-size: 25px;
-  padding: 15px;
-  margin-bottom: 15px;
-}
-.social-media-list img {
-  border-radius: 50%;
-  width: 50px;
-  height: 50px;
-  transition: transform 0.2s ease-in-out;
-}
+
+  .social-media-list a {
+    text-decoration: none;
+  }
+  .contact-text.phone a,
+  .contact-text.gmail a {
+    color:  rgb(67, 192, 190);
+    display: inline-block;
+    padding: 8px;
+    border-radius: 5px;
+    text-decoration: none;
+  }
 </style>
